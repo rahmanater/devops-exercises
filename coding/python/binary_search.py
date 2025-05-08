2ad00a17a4f2e288f6c2f344f7e286c5ba6db16e
rahmanater <41375500+rahmanater@users.noreply.github.com>
2025-05-08 08:54:30 +0100

    Update binary_search.py
    new

--- coding/python/binary_search.py
+++ coding/python/binary_search.py
@@ -1,28 +1,106 @@
-#!/usr/bin/env python
+from response_writer import *
+from acquire_request import *
+import logging
+import os
 
-import random
-from typing import List
-
-
-def binary_search(arr: List[int], lb: int, ub: int, target: int) -> int:
-    """
-    A Binary Search Example which has O(log n) time complexity.
-    """
-    if lb <= ub:
-        mid: int = lb + (ub - lb) // 2
-        if arr[mid] == target:
-            return mid
-        elif arr[mid] < target:
-            return binary_search(arr, mid + 1, ub, target)
-        else:
-            return binary_search(arr, lb, mid - 1, target)
-    else:
-        return -1
+logger = logging.getLogger()
+logging.basicConfig(level=os.environ.get("LOGLEVEL"))
 
 
-if __name__ == '__main__':
-    rand_num_li: List[int] = sorted([random.randint(1, 50) for _ in range(10)])
-    target: int = random.randint(1, 50)
-    print("List: {}\nTarget: {}\nIndex: {}".format(
-        rand_num_li, target,
-        binary_search(rand_num_li, 0, len(rand_num_li) - 1, target)))
+def validate_request(req):
+
+    # check the folder exists, i.e. the product
+    try:
+        logging.debug(f'req product id: {req.product_id}')
+        logging.debug(f'req csp id: {req.csp_id}')
+        with open("./catalogue/" + req.ds_id + ".json",'r', encoding='ISO-8859-1') as catalogue_load:
+            catalogue_file = catalogue_load.read()
+        catalogue = json.loads(catalogue_file)
+        #logger.info(catalogue)
+        for element in catalogue['cspDisclosureCatalogue']['cspDisclosureProduct']:
+
+            #for value in ['cspDisclosureCatalogue']['cspDisclosureProduct'][0]:
+            for attribute, value in element.items():
+                if attribute == "cspDisclosureProductID":
+                    #logger.info(value)
+                    if value.replace(':',"") == req.product_id:
+                        logger.info("Doing specific validation checks for DataRetain")
+                        if productOutputsAreValid(req.initial_req,req.product_id,element) == False:
+                            return write_error_response(400, "Invalid cspOutputDataTypeID", "THIS IS AN ERROR YOU WILL GET IF INCOMPATIBLE WITH DATARETAIN, CONTACT NCDS")
+                        if productFormatsAreValid(req.initial_req,req.product_id,element) == False:
+                            return write_error_response(400, "Invalid optionalResultFormatID", "THIS IS AN ERROR YOU WILL GET IF INCOMPATIBLE WITH DATARETAIN, CONTACT NCDS")
+        result_folder = "./result_files/{}/{}".format(req.csp_id,req.product_id)
+        logger.info(result_folder)
+        #for f in os.scandir(result_folder):
+            #logger.info(f.path)
+
+        store_request(req)
+
+    except FileNotFoundError as e:
+        logger.info("requested product does not exist")
+        #return write_error_response(400, "Invalid Product ID", "Unable to find a Disclosure Product with the specified ID: {}".format(req.product_id))
+        return False
+    except AttributeError as e:
+        logger.info("requested product does not exist")
+        #return write_error_response(400, "Invalid Product ID", "Unable to find a Disclosure Product with the specified ID: {}".format(req.product_id))
+        return False
+    finally:
+        store_request(req)
+
+    return True
+
+def new_request_handler(body,path):
+    # TODO check schema
+
+    # Have learnt there is no python Schema validator
+    # will need to be manually implemented against a specific schema version
+    try:
+        req = AcquireRequest(path, body)
+        logger.info(req.to_string)
+        return req
+    except AssertionError as inv:
+        logger.info("Request was invalid")
+        return write_error_response(400, "Invalid Request", "Schema validation failed: {}".format(inv.args[0]))
+
+#This code is here to mimic data retain
+def productOutputsAreValid(req,productId,productSchema):
+    cspOutputDataTypeName = []
+    for element in productSchema['productOutput']:
+        logger.info("Element productOutput")
+        logger.info(element)
+        if not element['optional']:
+            cspOutputDataTypeName.append(element["cspOutputDataTypeID"])
+
+    logger.info("cspOutputDataTypeName")
+    logger.info(cspOutputDataTypeName)
+    jsonReq = json.loads(req)
+    if "cdOptionalOutput" in jsonReq['cdAcquisitionRequest']['cdRequestDetails']:
+        for outputElement in jsonReq['cdAcquisitionRequest']['cdRequestDetails']['cdOptionalOutput']:
+            logger.info("OutputElement")
+            logger.info(outputElement)
+            if outputElement['cspOutputDataTypeID'] in cspOutputDataTypeName:
+                logger.info("Request is NOT compatible with DataRetain")
+                return False
+    logger.info("Request is compatible with DataRetain")
+    return True
+
+#This code is here to mimic data retain
+def productFormatsAreValid(req,productId,productSchema):
+    cspFormatTypeName = []
+    for element in productSchema['supportedResultFormat']:
+        logger.info("Element supportedResultFormat")
+        logger.info(element)
+        if not element['optional']:
+            cspFormatTypeName.append(element["resultFormatID"])
+
+    logger.info("cspFormatTypeName")
+    logger.info(cspFormatTypeName)
+    jsonReq = json.loads(req)
+    for outputElement in jsonReq['cdAcquisitionRequest']['cdRequestDetails']['optionalResultFormatID']:
+        logger.info("optionalResultFormatID")
+        logger.info(outputElement)
+        if outputElement in cspFormatTypeName:
+            logger.info("Request is NOT compatible with DataRetain")
+            return False
+    logger.info("Request is compatible with DataRetain")
+    return True
